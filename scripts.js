03d8f66f320b0c61c4f971b55a3620e37ec54a37
Front-End <alikeremovsahin@gmail.com>
2025-05-07 22:28:34 +0400

    JS 7th lesson

---
+++ scripts.js
@@ -0,0 +1,34 @@
+//Task2
+const pattern = /[a-zA-Z]/g;
+
+function numCounter(history) {
+    let letters = String(history).match(pattern).length;
+    return letters;
+
+}
+console.log(numCounter("Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, qui."));
+
+//Task3
+function capitalizeWords(text) {
+    let words = String(text).split(" ");
+    let capitalize = words[0].slice(0, 1).toUpperCase() + words[0].slice(1) + " " + words[1].slice(0, 1).toUpperCase() + words[1].slice(1)
+    return capitalize;
+}
+
+console.log(capitalizeWords("hello world"));
+
+//Task4
+function isLucky(number) {
+    let hexacopter = String(number).split("").slice(0, 6);
+
+    if (+hexacopter[0] + hexacopter[1] + hexacopter[2] === hexacopter[3] + hexacopter[4] + hexacopter[5]) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+console.log(isLucky(121121));
+console.log(isLucky(111191));
